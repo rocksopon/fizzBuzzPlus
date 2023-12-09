cf996c14a0f75dc1e78c2d478535684a9778b8f4
Sopon Jamreankit <soponj@th.dbs.com>
2023-12-09 21:29:13 +0700

    initial fizzBuzzPlus project

---
+++ src/main/java/com/bay/fizzBuzzPlus/FizzBuzzPlusApplication.java
@@ -0,0 +1,13 @@
+package com.bay.fizzBuzzPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bay/fizzBuzzPlus/FizzBuzzPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bay.fizzBuzzPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
