406f703598ed02ed597d2c920f61e1e705d36426
Sopon Jamreankit <soponj@th.dbs.com>
2023-12-10 13:50:17 +0700

    FizzBuzzPlus : Adding 1st test case

--- src/test/java/com/bay/fizzBuzzPlus/FizzBuzzPlusApplicationTests.java
+++ src/test/java/com/bay/fizzBuzzPlus/FizzBuzzPlusApplicationTests.java
@@ -3,11 +3,14 @@ package com.bay.fizzBuzzPlus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class FizzBuzzPlusApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void mainTest() {
+		assertDoesNotThrow(() -> FizzBuzzPlusApplication.main(new String[]{}));
 	}
 
 }
